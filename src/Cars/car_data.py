f89c8de3fa251f39879a064205e877b59babfe5a
dnp987 <dnp987@gmail.com>
2022-07-06 12:14:33 -0400

    Initial check in.

---
+++ src/Cars/car_data.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul. 6, 2022
+
+@author: dpenn
+'''
+class CarData:
+
+    def __init__(self, dealer_id, year, make, model, price, stock_num, link):
+        self.dealer_id = dealer_id
+        self.year = year
+        self.make = make
+        self.model = model
+        self.price = price
+        self.stock_num = stock_num
+        self.link = link
