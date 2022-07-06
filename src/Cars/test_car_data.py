69c98ba100b4c9eec40cb09903fb1fdffdcf608c
dnp987 <dnp987@gmail.com>
2022-07-06 15:00:10 -0400

    On going changes

--- src/Cars/test_car_data.py
+++ src/Cars/test_car_data.py
@@ -5,11 +5,12 @@ Created on Jul. 6, 2022
 '''
 from Cars.car_data import CarData
 if __name__ == '__main__':
-    used_car = CarData("   ID1", 1997, "  Ford  ", "    Taurus    ", "   $17,999    ", "   abc123   ", "   https://google.com    ")
-    print(used_car.dealer_id)
-    print(used_car.year)
-    print(used_car.make)
-    print(used_car.model)
-    print (used_car.price)
-    print(used_car.stock_num)
-    print(used_car.link)
+    def print_car_data(data):
+        print(data.dealer_id, " " , data.year, " ", data.make, " ", data.model, " " , data.price," " , data.stock_num, " ", data.link) 
+    
+    used_car1 = CarData("   ID1", 1997, "  Ford  ", "    Taurus    ", "   $17,999    ", "   abc123   ", "   https://google.com    ")
+    print_car_data(used_car1)
+    
+    used_car2 = CarData("   ID2 ", 2017, "  GM  ", "    Impala    ", "   $21,359    ", "   def456   ", "   https://google.com    ")
+    print_car_data(used_car2)
+    
