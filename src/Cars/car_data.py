64e4ac2e9ae55459c7d8b3473254474b20b01e24
dnp987 <dnp987@gmail.com>
2022-07-06 14:26:37 -0400

    Continuing development

--- src/Cars/car_data.py
+++ src/Cars/car_data.py
@@ -3,13 +3,14 @@ Created on Jul. 6, 2022
 
 @author: dpenn
 '''
+import re
 class CarData:
-
+# Remove all leading and trailing spaces from text data, and remove $ and comma from price
     def __init__(self, dealer_id, year, make, model, price, stock_num, link):
-        self.dealer_id = dealer_id
+        self.dealer_id = dealer_id.strip()
         self.year = year
-        self.make = make
-        self.model = model
-        self.price = price
-        self.stock_num = stock_num
-        self.link = link
+        self.make = make.strip()
+        self.model = model.strip()
+        self.price = (re.sub("[$,]", "", price)).strip()
+        self.stock_num = stock_num.strip()
+        self.link = link.strip()
