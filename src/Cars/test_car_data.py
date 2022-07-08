31d74f99730c157822d9a3150d7e209e8d130cda
dnp987 <dnp987@gmail.com>
2022-07-08 16:49:24 -0400

    Added test for arrays

--- src/Cars/test_car_data.py
+++ src/Cars/test_car_data.py
@@ -8,9 +8,16 @@ if __name__ == '__main__':
     def print_car_data(data):
         print(data.dealer_id, " " , data.year, " ", data.make, " ", data.model, " " , data.price," " , data.stock_num, " ", data.link) 
     
+    TestData = []
+    
     used_car1 = CarData("   ID1", 1997, "  Ford  ", "    Taurus    ", "   $17,999    ", "   abc123   ", "   https://google.com    ")
     print_car_data(used_car1)
     
     used_car2 = CarData("   ID2 ", 2017, "  GM  ", "    Impala    ", "   $21,359    ", "   def456   ", "   https://google.com    ")
     print_car_data(used_car2)
     
+    TestData.append(used_car1)
+    TestData.append(used_car2)
+    
+    for obj in TestData:
+        print_car_data(obj)
