72eac58ba9c0107204f37c021acfa2a53cc3300e
dnp987 <dnp987@gmail.com>
2022-07-08 16:49:11 -0400

    Added adjustments for price

--- src/Cars/car_data.py
+++ src/Cars/car_data.py
@@ -4,7 +4,7 @@ Created on Jul. 6, 2022
 @author: dpenn
 '''
 import re
-class CarData:
+class CarData():
 # Remove all leading and trailing spaces from text data, and remove $ and comma from price
     def __init__(self, dealer_id, year, make, model, price, stock_num, link):
         self.dealer_id = dealer_id.strip()
@@ -12,5 +12,7 @@ class CarData:
         self.make = make.strip()
         self.model = model.strip()
         self.price = (re.sub("[$,]", "", price)).strip()
+        if (not self.price.isdigit()): # if the price is "Please call" or something non-numeric, set the price to 0
+            self.price = '0'
         self.stock_num = stock_num.strip()
         self.link = link.strip()
