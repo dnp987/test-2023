45538f2ede631daa630f8cbc4f1076fda975e7e2
dnp987 <dnp987@gmail.com>
2022-07-11 13:47:39 -0400

    On going development

--- src/Cars/test_car_data.py
+++ src/Cars/test_car_data.py
@@ -11,13 +11,19 @@ if __name__ == '__main__':
     TestData = []
     
     used_car1 = CarData("   ID1", 1997, "  Ford  ", "    Taurus    ", "   $17,999    ", "   abc123   ", "   https://google.com    ")
-    print_car_data(used_car1)
-    
     used_car2 = CarData("   ID2 ", 2017, "  GM  ", "    Impala    ", "   $21,359    ", "   def456   ", "   https://google.com    ")
-    print_car_data(used_car2)
+    used_car3 = CarData
+    used_car3.dealer_id = " ABC123 "
+    used_car3.year = 1996
+    used_car3.make = "Toyota"
+    used_car3.model = "Corola"
+    used_car3.price = "$13,299 "
+    used_car3.stock_num = "defkas789sds"
+    used_car3.link = "https://toyota.ca"       
     
     TestData.append(used_car1)
     TestData.append(used_car2)
+    TestData.append(used_car3)
     
     for obj in TestData:
         print_car_data(obj)
