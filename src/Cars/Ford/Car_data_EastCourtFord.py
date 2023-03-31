9a7414d706343a52a391de18a78d67cf30de80f4
dnp987 <dnp987@gmail.com>
2023-03-31 17:24:41 -0400

    Updated since the web page changed.

--- src/Cars/Ford/Car_data_EastCourtFord.py
+++ src/Cars/Ford/Car_data_EastCourtFord.py
@@ -5,14 +5,13 @@ Since the next page icon is not easily readable, the total number of cars and ca
 '''
 from datetime import datetime
 import re
-#from selenium import webdriver
+from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Quotes.Excel_utils2 import Excel_utils2
 from Cars.CreateDealerSheet2 import CreateDealerSheet
 from Cars.browser_start import browser_start
-from Cars.car_data import CarData
 
 if __name__ == '__main__':
     file_in = 'C:/Users/dpenn/Desktop/Cars/CarData.xlsx'
@@ -24,67 +23,61 @@ if __name__ == '__main__':
     date_time = datetime.now().strftime('%Y %B %d %I %M %p') # get the date and time
     data_out = Excel_utils2(' ', date_time, 'out') # set the spreadsheet tab to the dealer name
     driver = browser_start(base_url, True) # run browser in headless mode if True
-    #driver = browser_start(base_url, False) # run browser in headless mode
+    #driver = browser_start(base_url, False)
     wait = WebDriverWait(driver, 10)
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.total-results-preview'))) # wait for number of cars to be displayed
     print (driver.title)
+    
+    # Close annoying pop-up if it appears
+    test1 = driver.find_element(By.CSS_SELECTOR, '.popup-close')
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.popup-close')
+    except:
+        pass
+    
     #test1 = driver.find_element_by_css_selector('span.text-lg').text  #can also find number of car with this
     num_cars = int(re.sub("[^0-9]", '', driver.find_element(By.CSS_SELECTOR,'div.total-results-preview').text))
     print ("Number of cars found on site: " , num_cars)
-    cars_per_page = 50
-    num_pages = num_cars // cars_per_page
-    if num_cars % cars_per_page >0: # if the number of cars per page doesn't divide evenly into the total number of cars, one more page is needed
-        num_pages += 1
     count = 0
     zero = 0
     car_info = []
-    print ("# of pages: ", num_pages)
 
-    for page in range(num_pages):
-        if page >0:
-            url = base_url[:-1] + str(page+1) # remove the last character of the base url and add the next page number
-            print (url)
-            driver.get(url) # Navigate to the test website
-            # wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".lg\:leading-tight"))) # wait for number of cars to be displayed
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.total-results-preview'))) # wait for number of cars to be displayed
-            #sleep(4)
-
+    pages_remaining = True
+    while pages_remaining:
         car_desc_raw = driver.find_elements(By.CSS_SELECTOR, 'h6' '.vehicle-title')
         prices = driver.find_elements(By.TAG_NAME, 'strong')
         stock = driver.find_elements(By.CSS_SELECTOR, 'div.w-full.text-center.font-light.text-sm.py-1.mt-2.md\:flex.md\:flex-wrap.md\:justify-center > div > p')
         details_links = driver.find_elements(By.CSS_SELECTOR, '.mx-auto.flex [href]')
-        car_temp = []
-                           
         for index, car in enumerate(car_desc_raw):
-            car_temp2 = CarData
-            car_temp2.dealer_id = ' '.join(dealer_id)
             car_name = (car.text +" ").split()[:4] # keep the year, make, and model, remove the rest
             year = car_name[0].split() # convert the year to a list
-            car_temp2.year = car_name[0]
             make = car_name[1].split() # convert make to a list
-            car_temp2.make = car_name[1]
             model = car_name[2:] # model is already a list
-            car_temp2.model = ' '.join(car_name[2:4])
             model = [' '.join(model)] # merge the model into one list element
-            #car_desc = year + make + model
-            car_desc = car_temp2.year + car_temp2.make + car_temp2.model
+            car_desc = year + make + model
             price = re.sub("[$,]", "", prices[index].text) # remove $ and ',' from the price
             if (not price.isdigit()): # if the price is "Please call" or something non-numeric, set the price to 0
                 price = '0'
                 zero += 1
             price = price.split() # convert to a list
-            car_temp2.price = prices[index].text
             stock_num = (stock[index].text[8:]).split() # remove "Stock #:" and keep the stock #
-            car_temp2.stock_num = stock[index].text[8:]
             link = (details_links[index].get_attribute('href')).split()
-            car_temp2.link = details_links[index].get_attribute('href') 
-            car_temp.append(car_temp2)
             print (index,":", car_desc, price, stock_num, link)
-            #car_info.append(dealer_id + car_desc + price + stock_num + link)
-            car_info.append(car_temp2.dealer_id + car_desc +car_temp2. price + car_temp2.stock_num + car_temp2.link)
+            car_info.append(dealer_id + car_desc + price + stock_num + link)
             count +=1
- 
-    print ("Total cars processed: ", count, " Total unpriced cars: ", zero)
+        
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'div.right-arrow').click()
+            sleep (10)
+            print ("*** Next page clicked ***")
+            print (driver.current_url)
+            print (base_url)
+            #wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.total-results-preview'))) # wait for number of cars to be displayed
+            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.selected-page'))) # wait for number of cars to be displayed
+            print ("next page found")
+        except:
+            print ("Total cars processed: ", count, " Total unpriced cars: ", zero)
+            pages_remaining = False
             
     car_info = sorted(car_info)
     for index, i in enumerate(car_info):
