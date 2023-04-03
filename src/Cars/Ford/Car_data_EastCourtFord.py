0e3116baad923d684b3364919e81ef8290b0c1e0
dnp987 <dnp987@gmail.com>
2023-04-03 17:35:55 -0400

    Updated, had to scroll to the bottom of the page to get the next page click to work.

--- src/Cars/Ford/Car_data_EastCourtFord.py
+++ src/Cars/Ford/Car_data_EastCourtFord.py
@@ -5,13 +5,21 @@ Since the next page icon is not easily readable, the total number of cars and ca
 '''
 from datetime import datetime
 import re
-from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Quotes.Excel_utils2 import Excel_utils2
 from Cars.CreateDealerSheet2 import CreateDealerSheet
 from Cars.browser_start import browser_start
+from Cars.Scroll_Browser import Scroll_Browser
+
+def check_pop_up(driver):
+    # Close annoying pop-up if it appears
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.popup-close').click()
+        print ("*** Annoying pop-up closed")
+    except:
+        pass
 
 if __name__ == '__main__':
     file_in = 'C:/Users/dpenn/Desktop/Cars/CarData.xlsx'
@@ -27,14 +35,7 @@ if __name__ == '__main__':
     wait = WebDriverWait(driver, 10)
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.total-results-preview'))) # wait for number of cars to be displayed
     print (driver.title)
-    
-    # Close annoying pop-up if it appears
-    test1 = driver.find_element(By.CSS_SELECTOR, '.popup-close')
-    try:
-        driver.find_element(By.CSS_SELECTOR, '.popup-close')
-    except:
-        pass
-    
+    check_pop_up(driver)
     #test1 = driver.find_element_by_css_selector('span.text-lg').text  #can also find number of car with this
     num_cars = int(re.sub("[^0-9]", '', driver.find_element(By.CSS_SELECTOR,'div.total-results-preview').text))
     print ("Number of cars found on site: " , num_cars)
@@ -44,6 +45,9 @@ if __name__ == '__main__':
 
     pages_remaining = True
     while pages_remaining:
+        scroll_pause_time = 0.5 # scroll all the way down until all pages are loaded
+        Scroll_Browser(driver, scroll_pause_time)
+        print (base_url, " - ", driver.current_url)
         car_desc_raw = driver.find_elements(By.CSS_SELECTOR, 'h6' '.vehicle-title')
         prices = driver.find_elements(By.TAG_NAME, 'strong')
         stock = driver.find_elements(By.CSS_SELECTOR, 'div.w-full.text-center.font-light.text-sm.py-1.mt-2.md\:flex.md\:flex-wrap.md\:justify-center > div > p')
@@ -65,28 +69,27 @@ if __name__ == '__main__':
             print (index,":", car_desc, price, stock_num, link)
             car_info.append(dealer_id + car_desc + price + stock_num + link)
             count +=1
-        
+        # try to get the next page of data if the right arrow is there
         try:
-            driver.find_element(By.CSS_SELECTOR, 'div.right-arrow').click()
-            sleep (10)
+            check_pop_up(driver)
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'svg.right-arrow-svg')))
+            next_page =  driver.find_element(By.CSS_SELECTOR, 'svg.right-arrow-svg')
+            next_page.click()
             print ("*** Next page clicked ***")
-            print (driver.current_url)
-            print (base_url)
-            #wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.total-results-preview'))) # wait for number of cars to be displayed
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.selected-page'))) # wait for number of cars to be displayed
-            print ("next page found")
         except:
-            print ("Total cars processed: ", count, " Total unpriced cars: ", zero)
+            print ("last page found")
             pages_remaining = False
             
+    print ("Total cars processed: ", count, " Total unpriced cars: ", zero)
     car_info = sorted(car_info)
     for index, i in enumerate(car_info):
         print (index, ":", i)
-        
-    print ("Saving data in a spreadsheet....", file_out)
-    CreateDealerSheet(data_out, car_info, date_time)
-    print (dealer, "Total cars: " , count)
-    data_out.save_file(file_out)
-       
-    driver.quit() # Close the browser and end the session
     
+    if (count != num_cars):
+        print ("Processing error. Number of cars expected: ", num_cars, " Number of cars found: " , count, " Data sheet not created.")
+    else:
+        print ("Saving data in a spreadsheet....", file_out)
+        CreateDealerSheet(data_out, car_info, date_time)
+        print (dealer, "Total cars: " , count)
+        data_out.save_file(file_out)
+    driver.quit() # Close the browser and end the session
