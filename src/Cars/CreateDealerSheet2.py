02d2fa2bd9b9f67037ccf4fbada3e7ca3b6fbc44
dnp987 <dnp987@gmail.com>
2023-04-03 15:17:11 -0400

    Minor changes

--- src/Cars/CreateDealerSheet2.py
+++ src/Cars/CreateDealerSheet2.py
@@ -3,7 +3,7 @@ Created on Jul 3, 2020
 
 @author: Home
 '''
-def CreateDealerSheet(SheetOut, car_info,date_time):
+def CreateDealerSheet(SheetOut, car_info, date_time):
     for row_index, car_line in enumerate(car_info, start = 1):
         for col_index, car_item in enumerate(car_line, start = 1):
             SheetOut.set_cell(row_index, col_index, car_item, "Arial", False, 10 )
