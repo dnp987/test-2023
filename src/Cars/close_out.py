2491f6615b133f1f4a999656210957af285cf266
dnp987 <dnp987@gmail.com>
2023-04-19 16:22:08 -0400

    Added more features from the main calling functions

--- src/Cars/close_out.py
+++ src/Cars/close_out.py
@@ -5,10 +5,16 @@ Created on Apr 11, 2023
 '''
 from Cars.CreateDealerSheet2 import CreateDealerSheet
 
-def close_out(driver, dealer, count, num_cars, data_out, file_out, date_time, car_info):
+def close_out(driver, dealer, count, zero, num_cars, data_out, file_out, date_time, car_info):
     if (count != num_cars):
         print ("Processing error. Number of cars expected: ", num_cars, " Number of cars found: " , count, " Data sheet not created.")
     else:
+        print ("Total cars processed: ", count, " Total unpriced cars: ", zero)
+        
+        car_info = sorted(car_info)
+        for index, car in enumerate(car_info):
+            print (index, ":", car)
+            
         print ("Saving data in a spreadsheet....", file_out)
         CreateDealerSheet(data_out, car_info, date_time)
         print (dealer, "Total cars: " , count)
